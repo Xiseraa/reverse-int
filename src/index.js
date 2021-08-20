11b1f3896a243f76d86f33ce00ae28bc372c2ec0
Artyom Khrystaforau <Xisera@mail.ru>
2021-08-20 04:46:36 +0300

    feat: try one more solution

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 module.exports = function reverse (n) {
+    n = n.toString();
     let result = '';
-    if(n.length === 1)
-    return n;
+    if(n.length === 1) {
+      return parseInt(n);
+    }
+    
     if(n < 0) {
         n = Math.abs(n);
         for(let i = 0; i < n.length; i++) {
             result = `${n[i]}${result}`
         }
-        return result;
+        return parseInt(result);
     }
     for(let i = 0; i < n.length; i++) {
         result = `${n[i]}${result}`
     }
-    return result;
+    return parseInt(result);
     
 }

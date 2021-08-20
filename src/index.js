c486be76dfaee86c70dc6af9b06565dc5e7e5159
Artyom Khrystaforau <Xisera@mail.ru>
2021-08-20 04:39:08 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    if(n.length === 1)
+    return n;
+    if(n < 0) {
+        n = Math.abs(n);
+        for(let i = 0; i < n.length; i++) {
+            result = `${n[i]}${result}`
+        }
+        return result;
+    }
+    for(let i = 0; i < n.length; i++) {
+        result = `${n[i]}${result}`
+    }
+    return result;
+    
 }

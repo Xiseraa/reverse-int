c5fc395b2fc3f29321e05a44ab0b6b597159ac16
Artyom Khrystaforau <Xisera@mail.ru>
2021-08-20 04:49:50 +0300

    fix: repair mistake with negative int

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 module.exports = function reverse (n) {
-    n = n.toString();
     let result = '';
+    if(n < 0) {
+        n = Math.abs(n);
+        n = n.toString();
+        for(let i = 0; i < n.length; i++) {
+            result = `${n[i]}${result}`
+        }
+      return parseInt(result);
+    }
+    n = n.toString();
+    
     if(n.length === 1) {
       return parseInt(n);
     }
     
-    if(n < 0) {
-        n = Math.abs(n);
-        for(let i = 0; i < n.length; i++) {
-            result = `${n[i]}${result}`
-        }
-        return parseInt(result);
-    }
+    
     for(let i = 0; i < n.length; i++) {
         result = `${n[i]}${result}`
     }
